ffefef146d773d5c5a038f9f6704d1c38150af65
Pavel Yudin <hazumida@gmail.com>
2022-04-22 19:03:19 +0400

    0.00001

--- src/com/company/Randoms.java
+++ src/com/company/Randoms.java
@@ -1,27 +1,21 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
-    private List<Integer> numbers = new ArrayList();
     private final int max, min;
 
     public Randoms(int min, int max) {
         this.random = new Random();
         this.max = max;
         this.min = min;
-        numbers.add(random.nextInt(max - min + 1) + min);
-
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
-            int nextIndex = 0;
 
             @Override
             public boolean hasNext() {
@@ -30,10 +24,8 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                int index = nextIndex;
-                nextIndex++;
-                numbers.add(random.nextInt(max - min + 1) + min);
-                return numbers.get(index);
+
+                return random.nextInt(max - min + 1) + min;
             }
         };
     }
